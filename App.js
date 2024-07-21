36b4f0d2379fe1b8422c6136bca3875839e780e0
HoangLoc <huynhngochoangloc0211102@gmail.com>
2024-07-21 16:33:53 +0700

    first start project

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationApp from './navigation/NavigationApp';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
-      <StatusBar style="auto" />
+      <NavigationApp />
     </View>
   );
 }
---
+++ navigation/NavigationApp.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import colors from '../constants/colors'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Welcome, Login} from '../screens/index'
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigationApp(props) {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Welcome">
+                <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
+                <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({})
