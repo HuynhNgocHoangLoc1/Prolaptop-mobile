9a301a0bd75e0d7f00b396c86c156affe9001ce5
HuynhNgocHoangLoc1 <huynhngochoangloc0211102@gmail.com>
2024-08-20 19:39:50 +0700

    router category to brand

--- navigation/NavigationApp.js
+++ navigation/NavigationApp.js
@@ -2,11 +2,20 @@ import { StyleSheet, Text, View, StatusBar } from "react-native";
 import React from "react";
 import colors from "../constants/colors";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Welcome, Login, SignUp, Home, Profile, Search, Cart } from "../screens/index";
+import {
+  Welcome,
+  Login,
+  SignUp,
+  Home,
+  Profile,
+  Search,
+  Cart,
+} from "../screens/index";
 import { NavigationContainer } from "@react-navigation/native";
 import UITab from "./UITab";
 import ProductDetail from "../screens/productDetail";
 import UpdateProfile from "../screens/profile/UpdateProfile";
+import Brand from "../screens/brand";
 
 
 const Stack = createNativeStackNavigator();
@@ -40,18 +49,23 @@ export default function NavigationApp(props) {
           component={UITab}
           options={{ headerShown: false }}
         />
-          <Stack.Screen
+        <Stack.Screen
           name="ProductDetail"
           component={ProductDetail}
           options={{ headerShown: true, title: "Product Detail" }}
         />
-       
-          <Stack.Screen
+
+        <Stack.Screen
           name="UpdateProfile"
           component={UpdateProfile}
           options={{ headerShown: true, title: "Update Profile" }}
           listeners={{ focus: () => StatusBar.setBarStyle("dark-content") }}
-
+        />
+        <Stack.Screen
+          name="Brand"
+          component={Brand}
+          options={{ headerShown: true, title: "Brand" }}
+          listeners={{ focus: () => StatusBar.setBarStyle("dark-content") }}
         />
       </Stack.Navigator>
     </NavigationContainer>
