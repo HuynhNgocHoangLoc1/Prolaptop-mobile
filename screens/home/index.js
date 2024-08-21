377d7febaacd944d19cf98223e8dc540e1c2d549
HuynhNgocHoangLoc1 <huynhngochoangloc0211102@gmail.com>
2024-08-21 10:28:02 +0700

    fake data avatar and name

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,15 +1,15 @@
-import { ScrollView, StyleSheet, Text, View, StatusBar } from "react-native";
+import { ScrollView, StyleSheet, Text, View, StatusBar, Image } from "react-native";
 import React from "react";
 import Slide from "./Slider";
 import Category from "./Category";
 import BestSeller from "./BestSeller";
 // import UITab from "../../navigation/UITab";
 import colors from "../../constants/colors";
-
+import fakeData from "../../fakeData/Data.json";
 
 export default function Home() {
+  const users = fakeData.user && fakeData.user.length > 0 ? fakeData.user : [0];
   return (
-    
     <ScrollView style={styles.container}>
        {/* <StatusBar
         animated={true}
@@ -17,15 +17,16 @@ export default function Home() {
         barStyle="dark-content"
       /> */}
       <View style={styles.header}>
-        <Text style={styles.textname}>Hi, user 1</Text>
+        <Text style={styles.textname}>Hi, {users[0].userName}</Text>
+        <Image source={{ uri: users[0].avatar }} style={styles.avatar} /> 
       </View>
 
       <View>
         <Slide style={styles.slider} />
       </View>
       <View style={styles.line}>
-      <Category />
-      <BestSeller />
+        <Category />
+        <BestSeller />
       </View>
      
       {/* <UITab style={styles} /> */}
@@ -37,17 +38,15 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "100%",
-
   },
   header: {
     width: "100%",
     height: 80,
-    resizeMode: "cover",
     backgroundColor: "#fff",
-    display: "flex",
-    transform: [{ scale: 1.0 }],
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row", // Align children in a row
+    alignItems: "center", // Center items vertically
+    paddingHorizontal: 16, // Add some horizontal padding
+    justifyContent: "space-between", // Space out the text and image
   },
   slider: {
     // Add your styles here
@@ -56,14 +55,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 28,
   },
-
   textname: {
     fontWeight: "bold",
     fontSize: 20,
     color: "black",
-    paddingRight: 290,
-    paddingTop: 0,
-    marginTop: 20,
+    marginTop: 30,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20, // Make the image circular
+    marginTop: 30,
   },
   line: {
     marginTop: 10,
